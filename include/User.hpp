53ab75c0de6090d57ead08ca128892d85a3ecba5
Lancelot Prieure <laprieur@3E6.42angouleme.fr>
2023-12-18 16:01:48 +0100

    Start slowly

--- include/User.hpp
+++ include/User.hpp
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_irc.hpp                                         :+:      :+:    :+:   */
+/*   User.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/14 14:08:20 by laprieur          #+#    #+#             */
-/*   Updated: 2023/12/18 14:23:27 by laprieur         ###   ########.fr       */
+/*   Created: 2023/12/18 15:56:15 by laprieur          #+#    #+#             */
+/*   Updated: 2023/12/18 16:01:25 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <netinet/in.h>
+#ifndef USER_HPP
+# define USER_HPP
 
-#define RED		"\033[31m"
-#define GREEN	"\033[32m"
-#define YELLOW	"\033[33m"
-#define BLUE	"\033[34m"
-#define NONE	"\033[0m"
+#include <string>
+
+class User {
+	private:
+		std::string	nickName;
+		std::string	userName;
+	
+	public:
+};
+
+#endif
