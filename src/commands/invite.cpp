a5248320412b29d64fad40a615e8aefa4302897d
Henri Sebille <hsebille@1H3.42angouleme.fr>
2024-01-15 14:35:35 +0100

    fixed invite command

--- src/commands/invite.cpp
+++ src/commands/invite.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   invite.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hsebille <hsebille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:51:16 by laprieur          #+#    #+#             */
-/*   Updated: 2024/01/15 10:28:38 by laprieur         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:33:53 by hsebille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,19 @@
 /* 482	ERR_CHANOPRIVSNEEDED	"<channel> :You're not channel operator"      */
 /* ************************************************************************** */
 
-static bool	parsing(const Client& client, const std::map<std::string, Channel>& channels, std::string cmd, std::string nickname, std::string channelName) {
+static bool	parsing(Server& server, const Client& client, const std::map<std::string, Channel>& channels, std::string cmd, std::string nickname, std::string channelName) {
 	if (nickname.empty() || channelName.empty())
 		Server::clientLog(client.getSocket(), ERR_NEEDMOREPARAMS(client.getUsername(), cmd));
-	else if (channels.find(channelName) == channels.end())
+	else if (channels.find(channelName) == channels.end()) {
+		std::cout << "Le channel" << channelName << " n'existe pas fdp" << std::endl;
 		return false;
+	}
 	Channel channel = channels.at(channelName);
 	if (!channel.isUser(client.getSocket()))
 		Server::clientLog(client.getSocket(), ERR_NOTONCHANNEL(client.getUsername(), channelName));
 	else if (!channel.isOperator(client.getSocket()))
 		Server::clientLog(client.getSocket(), ERR_CHANOPRIVSNEEDED(client.getUsername(), channelName));
-	else if (!findClient(channel.getMap(USERS), nickname))
+	else if (!findClient(server.getClients(), nickname))
 		Server::clientLog(client.getSocket(), ERR_NOSUCHNICK(client.getUsername(), nickname));
 	else if (findClient(channel.getMap(USERS), nickname))
 		Server::clientLog(client.getSocket(), ERR_USERONCHANNEL(client.getUsername(), client.getNickname(), channelName));
@@ -53,7 +55,7 @@ void	Server::invite(Client& client, const std::string& args) {
 	
 	iss >> nickname;
 	iss >> channel;
-	if (client.getRegistration() && parsing(client, _channels, "INVITE", nickname, channel)) {
+	if (client.getRegistration() && parsing(*this, client, _channels, "INVITE", nickname, channel)) {
 		for (std::map<int, Client>::const_iterator it = _clients.begin(); it != _clients.end(); it++)
 			if (it->second.getNickname() == nickname)
 				_channels.at(channel).addInvitee(it->second);
